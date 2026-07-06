e5e609e3d5ebc1b3ac215edf67a7953967f7d590
Tomek <tomasz.j.wesolowski@gmail.com>
2026-07-06 21:56:00 +0200

    Split pokemons to h and cpp files

---
+++ src/pokemons.cpp
@@ -0,0 +1,10 @@
+#include "battler.h"
+#include "elements.h"
+#include "pokemons.h"
+#include <memory>
+
+std::shared_ptr<Battler> make_machop() {
+  return std::make_shared<Battler>(std::string{"Machop"}, 70.0,
+                                   std::vector<Element>{Element::FIGHTING},
+                                   std::vector<Move>{});
+}
--- src/pokemons.h
+++ src/pokemons.h
@@ -1,11 +1,6 @@
 #pragma once
 
 #include "battler.h"
-#include "elements.h"
 #include <memory>
 
-std::shared_ptr<Battler> make_machop() {
-  return std::make_shared<Battler>(std::string{"Machop"}, 70.0,
-                                   std::vector<Element>{Element::FIGHTING},
-                                   std::vector<Move>{});
-}
+std::shared_ptr<Battler> make_machop();
